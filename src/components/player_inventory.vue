6888198b0c84f4e7f0ae4d8c3c201a0b33e71802
João Alves <joao.2003.couto@gmail.com>
2024-05-11 00:29:41 +0100

    forgot a file?

---
+++ src/components/player_inventory.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="absolute bottom-0 flex flex-row gap-8 w-full justify-center">
+        <div class="bg-purple-600 hover:bg-purple-800 p-8 h-32 flex items-center -rotate-12">card</div>
+        <div class="bg-purple-600 hover:bg-purple-800 p-8 h-32 flex items-center">card</div>
+        <div class="bg-purple-600 hover:bg-purple-800 p-8 h-32 flex items-center rotate-12">card</div>
+    </div>
+</template>
+
+<style scoped lang="postcss">
+
+</style>
